ed24eacb7ed45fda62d73cf88509ea4185d7a9f5
Tatyana Markova <tvmarko@yandex.ru>
2022-07-06 21:25:02 +0400

    14,

--- routes/cards.js
+++ routes/cards.js
@@ -19,19 +19,19 @@ router.post('/', celebrate({
 
 router.delete('/:cardId', celebrate({
   body: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().length(24).hex(),
   }),
 }), deleteCard);
 
 router.put('/:cardId/likes', celebrate({
   body: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().length(24).hex(),
   }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().length(24).hex(),
   }),
   headers: Joi.object().keys({
     authorization: Joi.string().required(),
